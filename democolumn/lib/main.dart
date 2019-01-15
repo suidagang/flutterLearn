0d4c1db41f55fde689cdded393510031eefa2543
suigang <244153653@qq.com>
2019-01-15 15:17:59 +0800

    完善
    Signed-off-by: suigang <244153653@qq.com>

--- democolumn/lib/main.dart
+++ democolumn/lib/main.dart
@@ -7,6 +7,18 @@ class MyApp extends StatelessWidget{
 			title:"suigang",
 			home:Scaffold(
 				appBar: new AppBar(title:new Text("demo-column")),
+				body: Center(child:Column(
+						crossAxisAlignment: CrossAxisAlignment.center,
+						mainAxisAlignment: MainAxisAlignment.center,
+						
+						children: <Widget>[
+							Text("第一行"),
+							Expanded(child:Text("第二行颠倒是非") ,) ,
+							Text("第三行"),
+							Text("第四行")
+						],
+					),
+				),
 			) ,
 		);
 	}
